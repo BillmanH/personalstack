cb104ef41420f00d800358af24411503a980caa5
billmanh <william.jeffrey.harding@gmail.com>
2022-06-03 09:27:02 -0700

    Removed blob store

--- infra/setting_local-env_vars.ps1
+++ infra/setting_local-env_vars.ps1
@@ -18,5 +18,5 @@ foreach($v in $json.PSObject.Properties)
     }
 }
 
-Write-Host "confirm keys set propperly with:"
+Write-Host "you can confirm keys set propperly with:"
 Write-Host "conda env config vars list"
